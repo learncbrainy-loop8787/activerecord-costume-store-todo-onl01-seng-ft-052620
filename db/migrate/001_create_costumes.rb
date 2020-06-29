049f088dcd13b436c0d9d05fdcd2ad47fcccf3c0
korey ko <koreykendrick1@gmail.com>
2020-06-29 18:17:35 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,3 +1,13 @@
 class CreateCostumes < ActiveRecord::Migration[5.2]
-  
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.integer :size
+      t.string :image_url
+    t.datetime :opening_time  
+    t.datetime :closing_time
+    t.timestamps
+  end
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
+
+  create_table "costume_stores", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "costume_inventory"
+    t.integer "num_of_employees"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
+    t.boolean "still_in_business"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.integer "price"
     t.integer "size"
     t.string "image_url"
-    t.integer "created_at"
-    t.integer "updated_at"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
