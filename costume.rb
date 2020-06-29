e32213c595e180cb495888251c4ea4506b22ee35
korey ko <koreykendrick1@gmail.com>
2020-06-29 14:40:19 +0000

    Done.

--- costume.rb
+++ costume.rb
@@ -1,2 +1,12 @@
-class Costume < ActiveRecord::Migration
+class Costume < ActiveRecord::Migration[5.2]
+   def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.integer :size
+      t.string :image_url
+      t.integer :created_at
+      t.integer :updated_at
+  end
+end
 end
--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,12 +1,3 @@
 class CreateCostumes < ActiveRecord::Migration[5.2]
-   def change
-    create_table :costumes do |t|
-      t.string :name
-      t.integer :price
-      t.integer :size
-      t.string :image_url
-      t.integer :created_at
-      t.integer :updated_at
-  end
-end
+  
 end
